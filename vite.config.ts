ff146ed274ff7bfbd0c3b100dbf0265736d4dfdd
TkymHrt <23.h.takayama.nutfes@gmail.com>
2025-06-27 17:01:42 +0900

    style: コードのフォーマットを統一し、不要なファイルを削除

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
-import { createRequire } from 'module'
-import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
+import { createRequire } from "node:module";
+import preact from "@preact/preset-vite";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    preact({
-      babel: {
-        // Change cwd to load Preact Babel plugins
-        cwd: createRequire(import.meta.url).resolve('@preact/preset-vite'),
-      },
-    }),
-  ],
-})
+	plugins: [
+		preact({
+			babel: {
+				// Change cwd to load Preact Babel plugins
+				cwd: createRequire(import.meta.url).resolve("@preact/preset-vite"),
+			},
+		}),
+	],
+});
